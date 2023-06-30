3b5e290396b04d44b8f94227a730bd1db22a9c3c
natenka <natasha.samoylenko@gmail.com>
2023-06-30 18:31:43 +0000

    UPD test 6.5

--- exercises/06_control_structures/test_task_6_5.py
+++ exercises/06_control_structures/test_task_6_5.py
@@ -90,7 +90,7 @@ def test_task_random_8_input_7_8(capsys, monkeypatch):
         "отримати потрібний результат, але й вивести його на стандартний потік "
         "виведення за допомогою print"
     )
-    assert "" in out and "" in out, (
+    assert "low" in out and "correct" in out, (
         f"При задуманому числі 8 та введення користувача 7, 8 на стандартному "
         f"потоці виводу повинен бути такий вивід {correct_stdout}"
     )
@@ -125,7 +125,7 @@ def test_task_random_4_input_1_1_1_1_1(capsys, monkeypatch):
         "отримати потрібний результат, але й вивести його на стандартний потік "
         "виведення за допомогою print"
     )
-    assert out.lower().count("") == 5 and "not guessed" in out.lower(), (
+    assert out.lower().count("low") == 5 and "not guessed" in out.lower(), (
         f"При задуманому числі 8 та введення користувача 1, 1, 1, 1, 1 на стандартному "
         f"потоці виводу повинен бути такий вивід {correct_stdout}"
     )
